1e12fb1e9fd4c0a1f92d40911e10ae511e29d702
Tony Bassotto <s193277@studenti.polito.it>
2016-03-28 23:59:31 +0200

    finale

--- Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/controller/Main.java
+++ Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/controller/Main.java
@@ -3,6 +3,7 @@ package it.polito.tdp.lab3.controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -12,10 +13,16 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("SegreteriaStudenti.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,primaryStage.getHeight(),primaryStage.getWidth());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("Segreteria Studenti");
+			primaryStage.setResizable(false);
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("logo.png")));
+			//Image applicationIcon = new Image(getClass().getResourceAsStream("logeo black-02.png"));
+			//primaryStage.getIcons().add(new Image("logeo black-02.png"));
+		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/controller/SegreteriaStudentiController.java
+++ Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/controller/SegreteriaStudentiController.java
@@ -1,5 +1,174 @@
 package it.polito.tdp.lab3.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.lab3.model.model;
+import it.polito.tdp.lab3.model.studente;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class SegreteriaStudentiController {
+	model m=new model();
 	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ChoiceBox<String> boxCorsi;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private Button btnCheck;
+
+    @FXML
+    private TextArea txtNome;
+
+    @FXML
+    private TextArea txtCognome;
+    
+    @FXML
+    private ImageView imgX;
+
+    @FXML
+    private Button btnFound;
+
+    @FXML
+    private Button btnSubmission;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doCheck(ActionEvent event) {
+    	try {String c=txtMatricola.getText();
+    	if(c.length()==0){
+    		txtNome.setText("v u o t o");
+    		txtCognome.setText("found");
+    		//Image x=new Image("glossy-grey-icon-button-hi.png");
+    		//imgX.setImage(x);
+    		}else if(txtMatricola.getText()!=null){
+    			studente s=m.checkStudent(c);
+    			if(s==null){
+    				txtNome.setText("not");
+    				txtCognome.setText("found");
+    				//Image x=new Image("glossy-grey-icon-button-hi.png");
+    				//imgX.setImage(x);
+    			
+    			}else{
+    				txtNome.setText(s.getNome());
+    				txtCognome.setText(s.getCognome());
+    				//Image x=new Image(getClass().getResourceAsStream("300px-Button_Icon_Green.svg.png"));//new Image(getClass().getResourceAsStream("logo.png"))
+    				//1btnCheck.setGraphic(new ImageView(x));
+    	    		}
+    		}
+    	}
+    		catch(NumberFormatException nfe){
+    			txtNome.setText("errore");
+
+    			txtCognome.setText("errore");
+    			//Image x=new Image("@../../../../../imh/glossy-grey-icon-button-hi.png");
+    			//imgX.setImage(x);
+    		
+    	}
+    		catch(Exception e){
+    			txtNome.setText(" ");
+
+    			txtCognome.setText(" ");
+    			//Image x=new Image("@../../../../../imh/glossy-grey-icon-button-hi.png");
+    			//imgX.setImage(x);
+    		
+    	}
+    	
+    }
+
+    @FXML
+    void doFound(ActionEvent event) {
+    	if(boxCorsi.getValue()==null && txtMatricola.getText().length()==0){
+    		txtResult.setText("Error 1004:\n Campi vuoti.");
+    	}else{
+    		try{
+    	
+    		if(boxCorsi.getValue()!=null && txtMatricola.getText().length()==0){
+    	
+    	String materia=boxCorsi.getValue();
+    	txtResult.setText(m.giveStudents(materia));
+    	}if(boxCorsi.getValue()==null && txtMatricola.getText().length()!=0){
+    		String matricola=txtMatricola.getText();
+    		txtResult.setText(m.giveMaterie(matricola));
+    	}else {
+    		String matricola=txtMatricola.getText();
+    		String materia=boxCorsi.getValue();
+    		txtResult.setText(m.presente(matricola,materia));
+    	}
+    		}
+    	
+
+    	
+    	catch(Exception e){
+    		txtResult.setText("Error 1001:\n Matricola emessa in maniera errata.");
+    	}
+    	}
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	txtNome.setText("");
+		txtCognome.setText("");
+		txtMatricola.setText("");
+		txtResult.setText("");
+		boxCorsi.setValue(null);
+
+    }
+
+    @FXML
+    void doSubmission(ActionEvent event) {
+    	try{
+    	if(boxCorsi.getValue()!=null && txtMatricola.getText().length()!=0){
+    		String matricola=txtMatricola.getText();
+    		String materia=boxCorsi.getValue();
+    		txtResult.setText(m.sub(matricola,materia));
+    	}
+    	}
+    	catch(Exception e){
+    		txtResult.setText("Error 1001:\n Matricola emessa in maniera errata.");
+    	}
+
+    }
+
+    @FXML
+    void initialize() {
+        assert boxCorsi != null : "fx:id=\"boxCorsi\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCheck != null : "fx:id=\"btnCheck\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnFound != null : "fx:id=\"btnFound\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnSubmission != null : "fx:id=\"btnSubmission\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        //assert imgX != null : "fx:id=\"imgX\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+
+       //ImageIcon x=new ImageIcon("E:/UNIVERSITA'/Programmazione a Oggetti/Lab3/logeo black-02.png");
+     
+        // btnCheck.setIcon(new ImageIcon("E:/UNIVERSITA'/Programmazione a Oggetti/Lab3/logeo black-02.png"));
+   
+       boxCorsi.getItems().addAll(m.corsi());
+        
+    }
 }
+
